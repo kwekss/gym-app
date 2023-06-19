810d0411fe169a0963b2a9ca3f20d59c7b0409f0
JohnAndrewsAsare <asarejohnandrews@gmail.com>
2023-06-19 00:59:19 +0000

    gym app with typescript

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import Navbar from "@/scenes/navbar";
+import { useState, useEffect } from "react";
+import { SelectedPage } from "@/shared/types";
+import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourClasses";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className=" app bg-gray-20 ">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        isTopOfPage={isTopOfPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
